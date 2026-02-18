fcfc764adbe90678ac9b46464381e8bf85ee8ede
Fatih Durmaz <mail.fatih.durmaz@gmail.com>
2026-02-18 11:11:52 +0000

    Added file check to nss. Nss config occasionally have config that is not installed in the system, which causes exceptions
    Signed-off-by: Fatih Durmaz <mail.fatih.durmaz@gmail.com>

--- analysis/app/src/nss.cpp
+++ analysis/app/src/nss.cpp
@@ -32,6 +32,7 @@
 
 #include "conductor/filesystem.h"
 #include "nss.h"
+#include <unistd.h>
 
 //#include "log.h"
 
@@ -150,6 +151,28 @@ void NSSFuncsPass::setupFuncs() {
     funcs["gethostbyname_r"] = "hosts";
     funcs["gethostbyaddr_r"] = "hosts";
 }
+static bool nssBackendExists(const std::string &backend) {
+    auto cfs = ConductorFilesystem::getInstance();
+    const std::string soname = "libnss_" + backend + ".so.2";
+
+    static const char *dirs[] = {
+        "/lib/x86_64-linux-gnu",
+        "/usr/lib/x86_64-linux-gnu",
+        "/lib",
+        "/usr/lib",
+        "/lib64",
+        "/usr/lib64",
+    };
+
+    for (const char *d : dirs) {
+        const std::string full =
+            cfs->transform(std::string(d) + "/" + soname);
+        if (access(full.c_str(), F_OK) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
 void NSSFuncsPass::loadConf() {
     std::ifstream conf(ConductorFilesystem::getInstance()
@@ -173,8 +196,15 @@ void NSSFuncsPass::loadConf() {
         std::string lib;
         while (ss >> lib) {
             if (lib[0] == '[') continue;
-            if(lib == "compat" || lib =="db" || lib == "dns" || lib == "files" || lib =="hesiod" || lib =="nis" || lib == "nisplus") //These are the libnss libraries provide in glibc-2.24
-                liblist.push_back(lib);
+            
+            if(lib == "compat" || lib == "db" || lib == "dns" || lib == "files" ||
+            lib == "hesiod" || lib == "nis" || lib == "nisplus") {
+
+                if (nssBackendExists(lib)) {
+                    liblist.push_back(lib);
+                }
+            }
+
             
         }
     }
